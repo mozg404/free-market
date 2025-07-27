f85540afa353b32510b11fa24721a07fbcb80fb5
mozg404 <masterdmx3325@gmail.com>
2025-07-27 19:50:05 +0500

    Добавлен App лайатут на фронте

---
+++ resources/js/layouts/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { Toaster, toast } from 'vue-sonner'
+import 'vue-sonner/style.css'
+import {usePage} from "@inertiajs/vue3";
+import {watch, computed} from "vue";
+
+const page = usePage()
+const toasts = computed(() => page.props.toasts)
+
+watch(
+  () => page.props.toasts,
+  (toasts) => {
+    if (!toasts?.length) {
+      return
+    }
+
+    toasts.forEach((toastData, index) => {
+      setTimeout(() => {
+        const params = {
+          description: toastData.description,
+        }
+
+        switch (toastData.type) {
+          case 'error': toast.error(toastData.message, params); break
+          case 'success': toast.success(toastData.message, params); break
+          case 'warning': toast.warning(toastData.message, params); break
+          case 'info': toast.info(toastData.message, params); break
+          default: toast(toastData.message, params)
+        }
+      }, index * 300)
+    })
+  },
+  {
+    deep: true,
+    immediate: true
+  }
+)
+
+</script>
+
+<template>
+  <Toaster closeButton richColors position="top-right" />
+  <slot/>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/layouts/Main.vue
+++ resources/js/layouts/Main.vue
@@ -1,51 +1,11 @@
 <script setup>
 import Header from "../components/Header.vue";
-import { Toaster, toast } from 'vue-sonner'
-import 'vue-sonner/style.css'
-import {usePage} from "@inertiajs/vue3";
-import {watch, computed} from "vue";
-
-const page = usePage()
-const toasts = computed(() => page.props.toasts)
-
-watch(
-  () => page.props.toasts,
-  (toasts) => {
-    if (!toasts?.length) {
-      return
-    }
-
-    toasts.forEach((toastData, index) => {
-      setTimeout(() => {
-        const params = {
-          description: toastData.description,
-        }
-
-        switch (toastData.type) {
-          case 'error': toast.error(toastData.message, params); break
-          case 'success': toast.success(toastData.message, params); break
-          case 'warning': toast.warning(toastData.message, params); break
-          case 'info': toast.info(toastData.message, params); break
-          default: toast(toastData.message, params)
-        }
-      }, index * 300)
-    })
-  },
-  {
-    deep: true,
-    immediate: true
-  }
-)
-
+import App from "@/layouts/App.vue";
 </script>
 
 <template>
-  <Toaster closeButton richColors position="top-right" />
-
-  <Header/>
-  <slot/>
+  <App>
+    <Header/>
+    <slot/>
+  </App>
 </template>
-
-<style scoped>
-
-</style>
