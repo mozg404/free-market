92d358a5056ffa1cdf616eb27251e83a937cd84a
mozg404 <masterdmx3325@gmail.com>
2025-08-29 20:41:59 +0500

    Исправлена ошибка при смене аватара пользователя

--- app/Http/Controllers/My/Settings/ChangeAvatarController.php
+++ app/Http/Controllers/My/Settings/ChangeAvatarController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\My\Settings;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MySettings\ChangeAvatarRequest;
+use App\Models\User;
 use App\Services\Toaster;
 use App\Services\User\AvatarChanger;
-use App\Support\Image;
 use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -17,7 +17,7 @@ class ChangeAvatarController extends Controller
     public function index(): Response
     {
         return Inertia::render('common/ImageUploaderModal', [
-            'imageUrl' => auth()->user()->avatar_url,
+            'imageUrl' => auth()->user()->getFirstMediaUrl(User::MEDIA_COLLECTION_AVATAR),
             'aspectRatio' => 1,
             'saveRoute' => route('my.settings.change.avatar.update'),
         ]);
