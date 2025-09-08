496979dffefb4fdbbb07ef01ece3dc3c10d550c1
mozg404 <masterdmx3325@gmail.com>
2025-09-08 11:49:59 +0500

    Добавлена джоба на создание рандомного пользователя

---
+++ app/Jobs/CreateDemoUser.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Services\User\DemoUserCreator;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class CreateDemoUser implements ShouldQueue
+{
+    use Queueable;
+
+    public function handle(DemoUserCreator $creator): void
+    {
+        $creator->create(
+            name: fake()->userName(),
+            email: fake()->unique()->email(),
+            password: config('demo.random_user_password'),
+            avatarPath: fake()->randomElement(include resource_path('data/user_avatars.php')),
+        );
+    }
+}
