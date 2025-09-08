e4092fa658178959ed12162a4c069e032f8a1043
mozg404 <masterdmx3325@gmail.com>
2025-09-08 22:52:44 +0500

    Fix планировщика

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,7 +9,7 @@ use App\Jobs\CreateRandomUser;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\Schedule;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
