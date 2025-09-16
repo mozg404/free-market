278a9ae5be6207232824b833dbe40af28ad3cab3
mozg404 <masterdmx3325@gmail.com>
2025-09-16 12:56:46 +0500

    Создан отдельный ProductData для использования внутри других Data объектов. Так как ProductForListingData идентичен ProductData - сделано наследование

---
+++ app/Data/Products/ProductData.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data\Products;
+
+use App\Data\User\UserData;
+use App\Enum\ProductStatus;
+use App\Support\Price;
+use Illuminate\Support\Carbon;
+use Spatie\LaravelData\Data;
+
+class ProductData extends Data
+{
+    public function __construct(
+        public int $id,
+        public string $name,
+        public Price $price,
+        public ?ProductPreviewData $preview,
+        public ProductStatus $status,
+        public ?UserData $user = null,
+        public Carbon $created_at,
+        public ?int $stock_items_count = null,
+        public ?int $available_stock_items_count = null,
+        public ?int $reserved_stock_items_count = null,
+    ) {
+    }
+}
